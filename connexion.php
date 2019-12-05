2c59689a6c5361df3ed76f4b18995881378e42b4
FlorenceT-G <57944788+FlorenceT-G@users.noreply.github.com>
2019-12-05 23:39:15 +0100

    commentaires

--- connexion.php
+++ connexion.php
@@ -3,42 +3,41 @@
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 	include_once("myparam.inc.php");
-	$_SESSION = null;
-	if(!empty($_POST))
+	$_SESSION = null; // les variables de sessions sont nulles avant la connexion de l'utilisateur
+	if(!empty($_POST)) // si les champs sont remplis
 	{
-		$pseudoError = null;
+		$pseudoError = null; // aucune erreur
 		$mdpError = null;
-		$pseudo = $_POST['pseudo'];
-		$mdp = $_POST['mdp'];
+		$pseudo = $_POST['pseudo']; // $pseudo fetch le pseudo entré dans l'input prévu à cet effet
+		$mdp = $_POST['mdp']; 	   // Pareil que pour pseudo
 		$valid = true;
-		if(empty($pseudo))
+		if(empty($pseudo)) // si les champs sont vides
 		{
 			$pseudoErreur = 'Veuillez entrer un pseudo.';
 			$valid = false;
 		}
-		if(empty($mdp))
+		if(empty($mdp)) 
 		{
 			$mdpErreur = 'Veuillez entrer un pseudo.';
 			$valid = false;
 		}
-		if($valid)
+		if($valid) // si les champs sont valides
 		{
-			$mdp = md5($mdp);
+			$mdp = md5($mdp); // on encrypte le mot de passe
 			$connect = mysqli_connect(MYHOST, MYUSER, MYPASS, DBNAME) or die ("Problème de connexion à la base de données");
-			$sql = "SELECT pseudo FROM personne WHERE pseudo = '".$pseudo."' AND mot_de_passe = '".$mdp."';";
+			$sql = "SELECT pseudo FROM personne WHERE pseudo = '".$pseudo."' AND mot_de_passe = '".$mdp."';"; // query
 			$requete = mysqli_query($connect,$sql);
-			if(mysqli_num_rows($requete) == 0)
+			if(mysqli_num_rows($requete) == 0) // si la query ne retourne rien, alors on soulève une erreur
 			{
 				echo "Le pseudo ou le mot de passe que vous avez entrés sont erronnés ou n'existent pas dans la base de données. Veuillez recommencer ou vous inscrire.";
 	        }
 	        else
 	        {
-				if(isset($_POST['pseudo']))
+				if(isset($_POST['pseudo'])) // sinon
 				{
-					session_start();
-	        		$_SESSION['pseudo'] = $pseudo;
-	        		echo("Vous êtes connecté.");
-	        		header('Location: ./accueil.php');
+					session_start(); // start la session
+	        			$_SESSION['pseudo'] = $pseudo; // on store le pseudo dans une variable de session
+	        			header('Location: ./accueil.php'); // redirige vers l'accueil
 				}
 			}
 		}
@@ -63,7 +62,8 @@
 
 	<body>
 	<div class="corps">
-	<h2>Connexion</h2>	
+	<h2>Connexion</h2>
+	<!-- Formulaire de connexion -->
 	<form action="#" method="post" class="formulaire">
 	<center>
 		<table>
