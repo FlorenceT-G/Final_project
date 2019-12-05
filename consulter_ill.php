02e35366d734622d5498ced9865dde41aafab2ce
FlorenceT-G <57944788+FlorenceT-G@users.noreply.github.com>
2019-12-05 23:19:30 +0100

    commentaire

--- consulter_ill.php
+++ consulter_ill.php
@@ -5,7 +5,7 @@
 ?>
 
 <?php
-    include_once "myparam.inc.php";
+    include_once "myparam.inc.php"; //inclusion du fichier de connexion à la base de donnée
     session_start();
 ?>
 
@@ -22,6 +22,7 @@
 	<body>
         <div class="corps">
             <h2>Consulter les espèces véhiculant des maladies</h2>
+            <!-- Formulaire permettant d'aller chercher toutes les maladies dans la table et de les afficher dans le menu déroulant -->
             <form action="" method="post" class="formulaire">
                 <div class="control-group <?php echo !empty($Error)?'error':'';?>">
                     <div>
@@ -52,13 +53,13 @@
             <center>
 
             <?php
-
-                if(!empty($_POST))
+                // si l'utilisateur a bien choisi un maladie à partir du menu déroulant alors on peut allez chercher dans la base de données les espèces pouvant potentiellement transmettre cette maladie et les affichées dans un tableau
+                if(!empty($_POST)) 
                 {
-                    $maladie = $_POST['maladie'];
-
-                    $base = mysqli_connect (MYHOST, MYUSER, MYPASS, DBNAME);
-                    $sql = "SELECT * FROM provoque NATURAL JOIN maladie WHERE nom_maladie = '".$maladie."';";
+                    $maladie = $_POST['maladie']; //récupère la sélection de l'utilisateur
+                    
+                    $base = mysqli_connect (MYHOST, MYUSER, MYPASS, DBNAME); // connexion à la base de donnée
+                    $sql = "SELECT * FROM provoque NATURAL JOIN maladie WHERE nom_maladie = '".$maladie."';"; // requête
                     $result = mysqli_query($base, $sql);
 
                     if(!$result)
@@ -66,7 +67,7 @@
                         echo("Error description: ".mysqli_error($base));
                     }
                     else
-                    {
+                    {   //tableau affichant les espèces véhiculant potentielle la maladie
                         echo "<h2>Maladie sélectionnée : ".$maladie."</h2>";
                         echo "<table class=\"tableau\">";
                             echo "<tr>";
